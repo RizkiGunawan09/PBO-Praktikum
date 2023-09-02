875e928a9c408f6e7fab7dc12b4f9fb0c6b45e29
RizkiGunawan09 <123919208+RizkiGunawan09@users.noreply.github.com>
2023-09-02 21:31:49 +0700

    Delete Constants2.java

--- Constants2.java
+++
@@ -1,9 +0,0 @@
-public class Constants2 {
-    public static final double CM_PER_INCH = 2.54; // Mendefinisikan konstanta kelas CM_PER_INCH dengan nilai 2.54
-    public static void main(String[] args) {
-        double paperWidth = 8.5; // Menginisialisasi lebar kertas dalam satuan inci
-        double paperHeight = 11; // Menginisialisasi tinggi kertas dalam satuan inci
-        // Menghitung dan mencetak ukuran kertas dalam sentimeter dengan mengalikan dengan konstanta kelas
-        System.out.println("Paper size in centimeters: " + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
-    }
-}
