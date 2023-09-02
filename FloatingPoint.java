626afed3ffea4db2f58b3844e0de6a455ff6b141
RizkiGunawan09 <123919208+RizkiGunawan09@users.noreply.github.com>
2023-09-02 21:32:03 +0700

    Delete FloatingPoint.java

--- FloatingPoint.java
+++
@@ -1,6 +0,0 @@
-public class FloatingPoint {
-    public static void main(String[] args) {
-        double x = 92.98; // Mendefinisikan variabel x dengan nilai 92.98 (bilangan desimal)
-        int nx = (int) Math.round(x); // Menggunakan Math.round() untuk membulatkan nilai x dan mengonversinya ke tipe int
-    }
-}
